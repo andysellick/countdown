eb02a06be1fe4c7774f62261cc312c02310ee077
andysellick <andy.sellick@gmail.com>
2020-02-14 16:52:33 +0000

    Improve code
    - namespace it all
    - make countdown number configurable

--- js/main.js
+++ js/main.js
@@ -1,77 +1,95 @@
-function resizeCircle(){
-  var pw = $(window).outerWidth();
-  var ph = $(window).outerHeight();
-  
-  var circle = Math.min(pw,ph) - 40;
-  $('#parent').css({
-    'width':circle + 'px',
-    'height':circle + 'px'
-  });
-}
+var countdown = {
+  interval: 0,
+  resize: 0,
 
-function rotateElement(elem, degrees){
-  elem.css({
-    'transform': 'rotate('+degrees+'deg)',
-    '-ms-transform': 'rotate('+degrees+'deg)',
-    '-moz-tranform': 'rotate('+degrees+'deg)',
-    '-webkit-transform': 'rotate('+degrees+'deg)',
-    '-o-transform': 'rotate('+degrees+'deg)'
-  });
-}
+  countdown_minutes: 5,
+  countdown_milliseconds: 0,
+  start_time: 0,
+  go_to: 0,
 
-function get_current_degrees(now, go_to) {
-  var time_so_far = now - start_time;
-  var percentage = (time_so_far / countdown_milliseconds) * 100;
-  var degrees = (percentage / 100) * 360;
-  return degrees;
-}
+  leftside: 0,
+  rightside: 0,
+  currentside: 0,
+  switched: 0,
 
-function minutes_left(now) {
-  return Math.ceil(countdown_minutes - ((now - start_time) / 60000));
-}
+  init: function(countdown_to) {
+    this.countdown_minutes = this.checkIsNumber(countdown_to);
+    this.countdown_milliseconds = this.countdown_minutes * 60000;
+    this.start_time = Date.now();
+    this.go_to = this.start_time + this.countdown_milliseconds;
+    this.leftside = $('#left_pane');
+    this.rightside = $('#right_pane');
+    this.currentside = this.rightside;
 
-var interval;
-var countdown_minutes = 5;
-var countdown_milliseconds = countdown_minutes * 60000;
-var start_time = Date.now();
-var go_to = start_time + countdown_milliseconds;
+    this.resizeCircle();
+    this.interval = setInterval(this.countdown, 100);
+    $('#countdown').html(this.minutesLeft(this.start_time));
 
-var leftside = $('#left_pane');
-var rightside = $('#right_pane');
-var currentside = rightside;
-var switched = 0;
+    $(window).on('resize',function(){
+      clearTimeout(this.resize);
+      this.resize = setTimeout(countdown.resizeCircle, 100);
+    });
+  },
 
-$(function() {
-  resizeCircle();
-  interval = setInterval(countdown, 100);
-  $('#countdown').html(minutes_left(start_time));
+  checkIsNumber: function(suspect) {
+    if (!isNaN(suspect) && suspect > 0) {
+      return Math.floor(suspect);
+    }
+    return 5;
+  },
 
-  var resize;
-  $(window).on('resize',function(){
-    clearTimeout(resize);
-    resize = setTimeout(resizeCircle, 200);
-  });
+  resizeCircle: function(){
+    var pw = $(window).outerWidth();
+    var ph = $(window).outerHeight();
 
-  function countdown() {
+    var circle = Math.min(pw,ph) - 40;
+    $('#parent').css({
+      'width':circle + 'px',
+      'height':circle + 'px'
+    });
+  },
+
+  rotateElement: function(elem, degrees){
+    elem.css({
+      'transform': 'rotate(' + degrees + 'deg)',
+      '-ms-transform': 'rotate(' + degrees + 'deg)',
+      '-moz-tranform': 'rotate(' + degrees + 'deg)',
+      '-webkit-transform': 'rotate(' + degrees + 'deg)',
+      '-o-transform': 'rotate(' + degrees + 'deg)'
+    });
+  },
+
+  getCurrentDegrees: function(now) {
+    var time_so_far = now - this.start_time;
+    var percentage = (time_so_far / this.countdown_milliseconds) * 100;
+    var degrees = (percentage / 100) * 360;
+    return degrees;
+  },
+
+  minutesLeft: function(now) {
+    return Math.ceil(this.countdown_minutes - ((now - this.start_time) / 60000));
+  },
+
+  countdown: function() {
     var now = Date.now();
-    if (now < go_to) {
-      var degrees = get_current_degrees(now, go_to);
+    if (now < countdown.go_to) {
+      var degrees = countdown.getCurrentDegrees(now);
 
-      if (degrees >= 180 && !switched) {
-        rotateElement(currentside, 180);
-        currentside = leftside;
-        switched = 1;
+      if (degrees >= 180 && !countdown.switched) {
+        countdown.rotateElement(countdown.currentside, 180);
+        countdown.currentside = countdown.leftside;
+        countdown.switched = 1;
       }
-      if (switched) {
+      if (countdown.switched) {
         degrees -= 180;
       }
-      rotateElement(currentside, degrees);
-      $('#countdown').html(minutes_left(now));
+      countdown.rotateElement(countdown.currentside, degrees);
+      $('#countdown').html(countdown.minutesLeft(now));
     }
     else {
-      clearInterval(interval);
-      rotateElement(currentside, 180);
+      clearInterval(countdown.interval);
+      countdown.rotateElement(countdown.currentside, 180);
       $('#countdown').html('0');
     }
   }
-});
+}
